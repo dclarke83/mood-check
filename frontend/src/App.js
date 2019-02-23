cf5f02e549033df1231a8055fc0b9fe564be18cd
Daniel Clarke <daniel.steven.clarke@googlemail.com>
2019-02-23 12:14:04 +0000

    Add empty insights page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,18 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Snackbar } from 'react-redux-snackbar';
 import NavBar from './NavBar';
 import CheckInPage from './CheckInPage';
+import InsightsPage from './InsightsPage';
 
 class App extends Component {
   render() {
     return (
-      // <div style={{backgroundColor: '#eee', height: '100%', position: 'absolute', top: '0', bottom: '0', right: '0', left: '0' }}>
       <div>
         <Router>
           <div>
             <NavBar />
             <div style={{ marginTop: '64px' }}>
               <Route exact path='/checkin' render={() => <CheckInPage /> } />
+              <Route exact path='/insights' render={() => <InsightsPage /> } />
             </div>
           </div>
         </Router>
---
+++ frontend/src/InsightsPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class InsightsPage extends Component {
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default InsightsPage;
