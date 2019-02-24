94cf1acc2d7309c8bddbcf36f913ae738896423e
Daniel Clarke <daniel.steven.clarke@googlemail.com>
2019-02-24 00:40:48 +0000

    Add mood face, doughnut, and totals to Insight Page

--- frontend/src/InsightsPage.js
+++ frontend/src/InsightsPage.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getMoods } from './redux/actions';
-import { getOrderedMoodHistory } from './redux/selectors';
+import { getMoodInsights } from './redux/selectors';
 import MoodEntry from './MoodEntry';
+import MoodFace from './MoodFace';
+import CircularProgressbar from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 class InsightsPage extends Component {
 
@@ -10,9 +13,31 @@ class InsightsPage extends Component {
         this.props.dispatch(getMoods());
     }
 
+    calcPercentage = () => {
+        return ((this.props.totals.score / this.props.totals.base) * 100).toFixed(1);
+    }
+
     render() {
         return (
             <div>
+                <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+                    <div style={{position: 'relative', margin: '30px'}}>
+                        <CircularProgressbar percentage={this.calcPercentage()}/>
+                        <div style={{position: 'absolute', top: '0', left: '0', height: '100%', width: '100%'}}>
+                            <div style={{display: 'flex', width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center' }}>
+                            <MoodFace perc={this.calcPercentage()} />
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{display:'flex', flexDirection:'column', alignContent:'center', justifyContent: 'center', alignItems: 'center'}}>
+                        <div style={{fontSize: '6em', fontWeight: 500 }}>
+                            {this.calcPercentage()}%
+                        </div>
+                        <div>
+                            Based on {this.props.totals.count} entries
+                        </div>
+                    </div>
+                </div>
                 <div>
                     <ol>
                     {this.props.orderedHistory.map(history => (
@@ -26,7 +51,7 @@ class InsightsPage extends Component {
 }
 
 const mapStateToProps = (state) => {
-    return getOrderedMoodHistory(state);
+    return getMoodInsights(state);
 }
 
 export default connect(mapStateToProps)(InsightsPage);
