921f14d682193f432d6277da0b5bd16a793072a4
Daniel Clarke <daniel.steven.clarke@googlemail.com>
2019-02-24 13:55:02 +0000

    Adding the option to show MoodFace in scaled colour

--- frontend/src/MoodFace.js
+++ frontend/src/MoodFace.js
@@ -22,14 +22,25 @@ const MoodFace = (props) => {
         color: '#cacaca',
         ...props.style
     };
+
+    const mergeStyles = (style, useColourScale = false, colour) => {
+        if(useColourScale){
+            return {
+                ...style,
+                color: colour
+            };
+        }
+        return style;
+    };
+
     const faces = [
-        { key: 1, start: 0, stop: 14.2, value: <FaRegTired style={iconStyle} /> },
-        { key: 2, start: 14.3, stop: 28.5, value: <FaRegAngry style={iconStyle} /> },
-        { key: 3, start: 28.6, stop: 42.8, value: <FaRegFrown style={iconStyle} /> },
-        { key: 4, start: 42.9, stop: 57.1, value: <FaRegMeh style={iconStyle} /> },
-        { key: 5, start: 57.2, stop: 71.4, value: <FaRegSmile style={iconStyle} /> },
-        { key: 6, start: 71.5, stop: 85.7, value: <FaRegSmileBeam style={iconStyle} /> },
-        { key: 7, start: 85.8, stop: 100, value: <FaRegGrinSquint style={iconStyle} /> }
+        { key: 1, start: 0, stop: 14.2, value: <FaRegTired style={mergeStyles(iconStyle, props.useColourScale, '#f00')} /> },
+        { key: 2, start: 14.3, stop: 28.5, value: <FaRegAngry style={mergeStyles(iconStyle, props.useColourScale, '#f95123')} /> },
+        { key: 3, start: 28.6, stop: 42.8, value: <FaRegFrown style={mergeStyles(iconStyle, props.useColourScale, '#fe9f2e')} /> },
+        { key: 4, start: 42.9, stop: 57.1, value: <FaRegMeh style={mergeStyles(iconStyle, props.useColourScale, '#fece38')} /> },
+        { key: 5, start: 57.2, stop: 71.4, value: <FaRegSmile style={mergeStyles(iconStyle, props.useColourScale, '#fff941')} /> },
+        { key: 6, start: 71.5, stop: 85.7, value: <FaRegSmileBeam style={mergeStyles(iconStyle, props.useColourScale, '#9df740')} /> },
+        { key: 7, start: 85.8, stop: 100, value: <FaRegGrinSquint style={mergeStyles(iconStyle, props.useColourScale, '#09f109')} /> }
     ];
 
     return (
